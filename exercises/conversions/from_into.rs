59db3e6653a72ba070c99086977b0357c1d83724
Teemu Miettunen <teemu.miettunen@outlook.com>
2021-09-12 23:48:21 +0300

    Everything should be done and working

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,30 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        match s.find(',') {
+            Some(position) => {
+                let name = &s[..position];
+                if name.len() == 0 {
+                    Person::default()
+                } else {
+                    let other = &s[(position + 1)..];
+                    match other.parse::<usize>() {
+                        Ok(n) => Person {
+                            name: name.to_string(),
+                            age: n,
+                        },
+                        _ => Person::default(),
+                    }
+                }
+            }
+            _ => Person::default(),
+        }
     }
 }
 
